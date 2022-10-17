e3514a21a9d3567f3fcf2a4ab6376596ef3f7a0e
AntoAda <adascalitiantonio@gmail.com>
2022-10-17 18:09:32 +0200

    es-array-cat-err completato

--- SO/es-array-cat-err/es-array-cat-err.c
+++ SO/es-array-cat-err/es-array-cat-err.c
@@ -1,23 +1,33 @@
+/* Manca l'import di stdio */
+#include <stdio.h>
+#include <string.h>
+
 /* Codice che concatena le due stringhe v1 e v2 */
 int main()
 {
-	char v1[10] = "Prima,"
-	char v2[10] = " dopo"
-	int len1, len2, i
-	
-	 /* Lunghezza delle due stringhe */
+	/* Mancano i ; */
+	char v1[10] = "Prima,";
+	char v2[10] = " dopo";
+	int len1, len2, i;
+
+	/* Lunghezza delle due stringhe */
 	len1 = sizeof(v1);
 	len2 = sizeof(v2);
 
+	/* Conto i caratteri della prima stringa per usarlo come offset*/
+	int l = len1 - strlen(v1);
+
 	/* Stampa delle due stringhe */
-	printf("%d\n", v1);
-	printf("%d\n", v2);	
+	/* Tipo d errato: per le stringhe si usa %s */
+	printf("%s\n", v1);
+	printf("%s\n", v2);
 
 	/* Copia la seconda stringa alla fine della prima */
-	for (i=0; i<len2; i++)
-		v1[len1+i] = v2[i];
-	
+	/* Devo farlo dalla posizione del terminatore, altrimenti saranno due stringhe diverse */
+	for (i = 0; i < len2; i++)
+		v1[len1 - l + i] = v2[i];
+
 	/* Stampa la stringa concatenata */
-	printf("%d\n", v1);
-	
+	/* Tipo d errato: per le stringhe si usa %s */
+	printf("%s\n", v1);
 }
